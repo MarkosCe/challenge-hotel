1db4364e60a5a22da6e84ca82151f474118a3e94
markosCe <cesar.markos99@gmail.com>
2023-09-09 15:27:50 -0600

    first commit

---
+++ src/main/java/com/alura/challengehotel/ChallengeJavaHotelA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alura.challengehotel;
+
+/**
+ *
+ * @author cesar
+ */
+public class ChallengeJavaHotelA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
